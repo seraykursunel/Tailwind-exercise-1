a69b313ba1d069405a1898008371ad1c85a3ba99
seraykursunel <seraykursunel@gmail.com>
2024-04-10 01:24:38 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import Image from 'next/image'
+
+
+function Avatar(props) {
+  return (
+    <div className='group block p-8 flex-shrink-0'>
+      <div className='flex items-center'>
+        <div>
+          <Image
+            className='inline-block h-12 w-12 rounded-full'
+            src={props.imageSrc}
+            alt=''
+            width={500}
+            height={500}
+          />
+        </div>
+        <div className='ml-3'>
+          <p className='text-base font-medium text-gray-700 group-hover:text-gray-900'>
+            {props.name}
+          </p>
+          <p className='text-sm font-medium text-gray-500 group-hover:text-gray-700'>
+            {props.profileText}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default function App() {
+  return (
+    <>
+  <Avatar profileText='Profile bakın' name="Tom Cook" imageSrc='/photo.png'/>
+  <Avatar profileText='Profile bakın' name="John Cook" imageSrc='/photo.png'/>
+  </>
+  )
+}
